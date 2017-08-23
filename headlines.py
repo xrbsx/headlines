56a9bd3255ccbbd48ad1eb9b2c895bcbf26d4e69
Rafael Barbosa <rafaelb.barbosa@gmail.com>
2017-08-23 16:43:17 -0300

    Utilizando rota dinamica

--- headlines.py
+++ headlines.py
@@ -4,15 +4,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+        'cnn': 'http://rss.cnn.com/rss/edition.rss',
+        'fox': 'http://feeds.foxnews.com/foxnews/latest',
+        'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+
+#@app.route("/bbc")
+#def bbc():
+#    return get_news('bbc')
+#
+#@app.route("/cnn")
+#def cnn():
+#    return get_news('cnn')
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route("/<publication>")
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return """<html>
         <body>
-            <h1> BBC Headlines </h1>
+            <h1> Headlines </h1>
             <b>{0}</b> <br/>
             <i>{1}</i> <br/>
             <p>{2}</p> <br/>
